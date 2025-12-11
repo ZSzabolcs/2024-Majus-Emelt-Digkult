f7098f78d9a221e993c3c8a292975b7bc3680ca1
ZSzabolcs <zelenaksz@kkszki.hu>
2025-12-11 12:16:08 +0100

    Első lépések

---
+++ ZelenakSz_13.A_belepteto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZelenakSz_13.A_belepteto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
